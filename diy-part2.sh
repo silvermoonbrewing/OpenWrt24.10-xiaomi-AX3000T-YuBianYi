484d40d3c4fdc3a667fad5b0791c9dbe7b3fdb30
BYLyz03w4p <lujunxi@msn.com>
2024-11-13 13:27:59 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -7,3 +7,4 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.89.1/g' package/base-files/files/bin/config_generate
+sed -i 's/hostname='OpenWrt'/hostname='OpenWrt-X86-X64-IPV6'/g' package/base-files/files/bin/config_generate
