643ce314a6a08d3455d104e636162e47938c0482
BYLyz03w4p <lujunxi@msn.com>
2024-11-23 23:25:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,27 +22,3 @@ sed -i "s/'UTC'/'CST-8'/g" package/base-files/files/bin/config_generate
 # 在时区设置后添加区域名称设置
 sed -i "/set system.\@system\[-1\].timezone='CST-8'/a\ \ \ \ set system.\@system\[-1\].zonename='Asia/Shanghai'" package/base-files/files/bin/config_generate
 
-# Step 5: 自定义网络配置
-# 创建网络配置文件并写入自定义配置
-mkdir -p package/base-files/files/etc/config  # 确保配置目录存在
-
-cat << EOF > package/base-files/files/etc/config/network
-config device
-    option name 'br-lan'
-    option type 'bridge'
-    list ports 'eth0'
-    list ports 'eth2'
-    list ports 'eth3'
-
-config interface 'lan'
-    option device 'br-lan'
-    option proto 'static'
-    option ipaddr '192.168.89.1'  # 默认 IP 地址
-    option netmask '255.255.255.0'
-
-config interface 'wan'
-    option device 'eth1'
-    option proto 'dhcp'
-EOF
-
-# 提示：以上配置适用于 X86 平台。若编译其他平台，请根据需求调整接口配置。
