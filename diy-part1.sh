a6e46720d46928b4c49ce10f2233b234ce67e3ff
BYLyz03w4p <lujunxi@msn.com>
2024-12-14 10:59:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,4 +10,4 @@
 
 
 
-sed -i '$a src-git kwrtpackages https://github.com/kiddin9/kwrt-packages.git' feeds.conf.default
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
